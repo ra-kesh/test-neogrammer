91d172026e4276224ca2bd8cf9db1d9ad5c13d19
Rakesh <iamrakeshkumar@pm.me>
2021-02-08 01:36:48 +0530

    next config update

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ module.exports = withCSS ({
     publicRuntimeConfig:{
         APP_NAME : 'NeoGrammers',
         API_DEVELOPMENT : 'http://localhost:8000/api',
-        API_PRODUCTION : 'https://protected-thicket-88416.herokuapp.com/',
+        API_PRODUCTION : 'https://protected-thicket-88416.herokuapp.com/api',
         PRODUCTION : true ,
         DISQUS_SHORTNAME : 'neogrammer',
-        DOMAIN_PRODUCTION : 'https://test-neogrammer.vercel.app/',
+        DOMAIN_PRODUCTION : 'something.com',
         DOMAIN_DEVELOPMENT : 'http://localhost:3000'
 
     }
