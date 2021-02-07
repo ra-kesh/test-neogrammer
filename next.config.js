e112dcd53437a55da02c8e97f1845dae361ec987
Rakesh <iamrakeshkumar@pm.me>
2021-02-08 01:01:31 +0530

    next config correction

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const withCSS = require('@zeit/next-css');
 module.exports = withCSS ({
     publicRuntimeConfig:{
         APP_NAME : 'NeoGrammers',
-        API_DEVELOPMENT : 'https://protected-thicket-88416.herokuapp.com/',
-        PRODUCTION : false ,
+        API_DEVELOPMENT : 'http://localhost:8000/api',
+        API_PRODUCTION : 'https://protected-thicket-88416.herokuapp.com/',
+        PRODUCTION : true ,
         DISQUS_SHORTNAME : 'neogrammer',
-        DOMAIN_PRODUCTION : 'something.com',
+        DOMAIN_PRODUCTION : 'https://test-neogrammer.vercel.app/',
         DOMAIN_DEVELOPMENT : 'http://localhost:3000'
 
     }
