b02868a5b6ede544397f1dbcb36d9dc91151a63a
Rakesh <iamrakeshkumar@pm.me>
2021-02-14 01:42:57 +0530

    changes in card again

--- components/project/Card.js
+++ components/project/Card.js
@@ -7,14 +7,14 @@ const Card = ({ project }) => {
     const showProjectCategories = project =>
         project.categories.map((c, i) => (
             <Link key={i} href={`/categories/${c.slug}`}>
-                <a className="btn btn-primary mr-1 ml-1 mt-3">{c.name}</a>
+                <a className="mark mr-1 ml-1 mt-3">{c.name}</a>
             </Link>
         ));
 
     const showProjectTags = project =>
         project.tags.map((t, i) => (
             <Link key={i} href={`/tags/${t.slug}`}>
-                <a className="btn btn-outline-primary mr-1 ml-1 mt-3">{t.name}</a>
+                <a className="pro-tag mr-1 ml-1 mt-3">{t.name}</a>
             </Link>
         ));
 
@@ -57,24 +57,12 @@ const Card = ({ project }) => {
             </section>
             <section>
                 <p className="mark ml-1 pt-2 pb-2">
-                    Devloped by  <Link href={`/profile/${project.postedBy.username}`}>
+                     by  <Link href={`/profile/${project.postedBy.username}`}>
                                             <a>{project.postedBy.username}</a>
                                         </Link> | Added {Moment(project.updatedAt).fromNow()}
                 </p>
             </section>
-            <div className="row">
-                <div className="col-md-4">
-                    {/* <section>
-                        <img
-                            className="img img-fluid"
-                            style={{ maxHeight: '150px', width: 'auto' }}
-                            src={`${API}/project/photo/${project.slug}`}
-                            alt={project.title}
-                        />
-                        
-                    </section> */}
-                </div>
-                <section>
+            <section>
                         <Link href={`/projects/${project.slug}`}>
                         <a>
                             <h2 className="pt-3 pb-3 font-weight-bold">{project.title}</h2>
@@ -89,10 +77,23 @@ const Card = ({ project }) => {
                         {showProjectTags(project)}
                         </div>
                     </section>
-                {/* <div className="col-md-8">
+            {/* <div className="row">
+                <div className="col-md-4">
+                    <section>
+                        <img
+                            className="img img-fluid"
+                            style={{ maxHeight: '150px', width: 'auto' }}
+                            src={`${API}/project/photo/${project.slug}`}
+                            alt={project.title}
+                        />
+                        
+                    </section>
+                </div>
+               
+                <div className="col-md-8">
                    
-                </div> */}
-            </div>
+                </div>
+            </div> */}
         </div>
     
         
