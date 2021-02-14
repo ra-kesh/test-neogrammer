cee7bb7d500237ccc429bfb230e7fd9458331c3b
Rakesh <iamrakeshkumar@pm.me>
2021-02-14 18:12:16 +0530

    tag page

--- pages/tags/[slug].js
+++ pages/tags/[slug].js
@@ -14,14 +14,14 @@ const Tag = ({ tag, projects, query }) => {
         <React.Fragment>
             <Layout>
                 <main>
-                    <div className="container-fluid row">
+                    <div className="container-fluid ">
                         <header>
                             <div className="div-project-header">
                                 <h1 className="display-4 font-weight-bold">{tag.name}</h1>
                                
                             </div>
                         </header>
-                        <div>
+                        <div className="row">
                         {projects.map((p, i) => (
                                     <div className="col-lg-4 pro-card">
                                         <Card key={i} project={p} />
