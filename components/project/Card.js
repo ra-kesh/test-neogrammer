2c58edc629c73471525bcef95ffaa2406819ce23
Rakesh <iamrakeshkumar@pm.me>
2021-02-14 15:49:09 +0530

    card design

--- components/project/Card.js
+++ components/project/Card.js
@@ -7,7 +7,7 @@ const Card = ({ project }) => {
     const showProjectCategories = project =>
         project.categories.map((c, i) => (
             <Link key={i} href={`/categories/${c.slug}`}>
-                <a className="mark mr-1 ml-1 mt-3">{c.name}</a>
+                <a className="mark m-2 p-2">{c.name}</a>
             </Link>
         ));
 
@@ -20,15 +20,7 @@ const Card = ({ project }) => {
 
     return (
       
-   <div className="card pb-4">
-            {/* <header>
-                <Link href={`/projects/${project.slug}`}>
-                    <a>
-                        <h2 className="pt-3 pb-3 font-weight-bold">{project.title}</h2>
-                    </a>
-                </Link>
-            </header> */}
-
+   <div className="card pb-4 p-2">
             <section>
                 <Link href={`/projects/${project.slug}`}>
                     <a>
@@ -41,45 +33,35 @@ const Card = ({ project }) => {
                     </a>
                 </Link>
             </section>
-          
-            
-            {/* <section>
-                        <img
-                            className="img img-fluid"
-                            style={{ maxHeight: '250px', width: '100%' }}
-                            src={`${API}/project/photo/${project.slug}`}
-                            alt={project.title}
-                        />
-            </section> */}
+       
             <section>
-                {showProjectCategories(project)}
-               
-            </section>
-           
-            <section>
-                        <Link href={`/projects/${project.slug}`}>
-                        <a>
-                            <h2 className="pt-3 pb-3 font-weight-bold">{project.title}</h2>
-                        </a>
-                        </Link>
-                        by  <Link href={`/profile/${project.postedBy.username}`}>
-                                            <a>{project.postedBy.username}</a>
-                              </Link> 
-                        {/* <div className="pb-3">{renderHTML(project.excerpt)}</div>
-                        <Link href={`/projects/${project.slug}`}>
-                            <a className="btn btn-primary pt-2">Read more</a>
-                        </Link> */}
-                        
+                   {showProjectCategories(project)}
+        
+                    <Link href={`/projects/${project.slug}`}>
+                    <a>
+                        <h2 className="pt-3 pb-3 font-weight-bold">{project.title}</h2>
+                    </a>
+                    </Link>
+
+                    <div className="card-author">
+                        <h4> by  <Link href={`/profile/${project.postedBy.username}`}>
+                                        <a>{project.postedBy.username}</a>
+                        </Link> </h4>
+                    
+                    </div>          
              </section>
              <section>
                 <div>
 
                 {showProjectTags(project)}
                 </div>
-                <p className="float-right">
+              
+            </section>
+            <div>
+            <p className="float-right">
                      Added {Moment(project.updatedAt).fromNow()}
                 </p>
-            </section>
+            </div>
 
             {/* <div className="row">
                 <div className="col-md-4">
