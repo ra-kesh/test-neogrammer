c875299a15a5652318ef1d6a1ef14162f3406af3
Rakesh <iamrakeshkumar@pm.me>
2021-02-15 13:34:57 +0530

    profile page updated

--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -5,6 +5,7 @@ import { userPublicProfile } from '../../actions/user';
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 import moment from 'moment';
 import ContactForm from '../../components/form/ContactForm';
+import { useRouter } from 'next/router';
 
 const UserProfile = ({ user, projects, query }) => {
 
@@ -12,9 +13,11 @@ const UserProfile = ({ user, projects, query }) => {
         return projects.map((project, i) => {
             return (
                 <div className="mt-4 mb-4" key={i}>
-                    <Link href={`/projects/${project.slug}`}>
+
+                <h6><Link href={`/projects/${project.slug}`}>
                         <a className="lead">{project.title}</a>
-                    </Link>
+                    </Link></h6>
+                    
                 </div>
             );
         });
@@ -25,15 +28,33 @@ const UserProfile = ({ user, projects, query }) => {
             {/* {head()} */}
             <Layout>
                 <div className="container">
-                <div className="row">
-                        <div className="col-md-12">
-                            <div className="card">
+                <div className="row pro-wrapper">
+                        <div className="col-lg-4">
+                            <div >
+                                <img
+                                    src={`${API}/user/photo/${user.username}`}
+                                    className="img img-fluid img-thumbnail mb-3"
+                                    style={{ maxHeight: '250px', maxWidth: '100%' , borderRadius:'50%'}}
+                                    alt="user profile"
+                                />
+                            </div>
+                            <div>
+                            <p className="text-muted">Joined {moment(user.createdAt).fromNow()}</p>
+                            </div> 
+                        </div>
+                        <div className="col-lg-6">
+                            <h4>{user.name}</h4>
+                            <div>
+                            <p className="text-muted">{user.about}</p>
+                            </div>
+                        </div>
+                        {/* <div className="card">
                                 <div className="card-body">
                                     <div className="row">
                                         <div className="col-md-8">
                                             <h5>{user.name}</h5>
-                                            <p className="text-muted">Joined {moment(user.createdAt).fromNow()}</p>
-                                            <p className="text-muted">{user.about}</p>
+                                          
+                                           
 
 
                                         </div>
@@ -47,20 +68,19 @@ const UserProfile = ({ user, projects, query }) => {
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            </div> */}
                     </div>
                 </div>
 
                 <br />
 
                 <div className="container pb-5">
-                    <div className="row">
-                        <div className="col-md-6">
+                    <div className="row pro-wrapper">
+                        <div className="col-lg-8">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title bg-primary pt-4 pb-4 pl-4 pr-4 text-white">
-                                        Recent projects by {user.name}
+                                    <h5 className="card-title bg-primary pt-4 pb-4 pl-4 pr-4 ">
+                                        All the projects built by {user.name}
                                     </h5>
 
                                     {showUserProjects()}
@@ -68,7 +88,7 @@ const UserProfile = ({ user, projects, query }) => {
                             </div>
                         </div>
 
-                        <div className="col-md-6">
+                        {/* <div className="col-md-6">
                             <div className="card">
                                 <div className="card-body">
                                     <h5 className="card-title bg-primary pt-4 pb-4 pl-4 pr-4 text-light">
@@ -78,7 +98,7 @@ const UserProfile = ({ user, projects, query }) => {
                                     <ContactForm authorEmail={user.email}/>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </Layout>
